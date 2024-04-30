f60de4750f53512e49714dc2a28817deb33de713
Ronald Espitia <ronald79@gmail.com>
2024-04-29 22:18:56 -0500

    first commit

---
+++ sofia.js
@@ -0,0 +1,47 @@
+//Tipos de Datos JS
+// number
+// strings
+// Booleanos
+
+//NUMBER
+// declaracion
+let variable;
+
+// inicializacion
+let numero = 101;
+
+// asignacion
+variable = 222;
+
+let pepito = 1000;
+const PI = 3.14;
+const PI2 = 3.14;
+
+// camelCase
+let notaPromedioSemestre1;
+
+// snake_case
+let nota_promedio_semestre;
+
+//console.log(numero + variable + pepito);
+// STRING
+let apellido = 'Rojas';
+
+let apellido2 = "Rojas";
+
+console.log(apellido + apellido2);
+console.log(apellido + ' Otro Apellido: ' + apellido2);
+
+//apellido = 100;
+
+console.log(apellido + variable);
+
+//BOOLEANOS
+let aprobo = false;
+
+console.log('Hola como estas\n \'Sofia\' ' + apellido + ' Otro Apellido: ' + apellido2);
+
+//Literal Template
+console.log(`Hola como estas\n'Sofia' ${apellido} otro Apellido: ${apellido2}`);
+
+
