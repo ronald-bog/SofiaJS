1a263badd1e6e5b280ec2c419ab2cee926367bea
Ronald Espitia <ronald79@gmail.com>
2024-06-06 21:05:59 -0500

    clase junio 6

--- junio4.js
+++ junio4.js
@@ -15,9 +15,19 @@ function avg() {
     return suma / qty;
 };
 
+// usando reduce
+//const avg = ratings.reduce((ac, i) => ac + i) / qty;
+
 const msg = (names, ratings) => {
     let approved = '';
     ratings.forEach((rating, i) => rating >= 60 ? approved += `${names[i]}: ${rating}\n` : '');
     return `El promedio de calificaciones es: ${avg()}\n\nEstudiantes aprobados:\n${approved}`;
 };
+
+// usando map
+// const msg = (names, ratings) => {
+//     let approved = ratings.map((rating, i) => rating >= 60 ? `${names[i]}: ${rating}\n` : '').join('');
+//     return `El promedio de calificaciones es: ${avg}\n\nEstudiantes aprobados:\n${approved}`;
+// };
+
 alert(msg(names, ratings));
