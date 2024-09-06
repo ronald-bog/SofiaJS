b806d7193db0ea80c2afdb6d79adb2189734e260
Ronald Espitia <ronald79@gmail.com>
2024-09-06 18:27:56 -0500

    Clase Sept 6

--- sept5_routes.js
+++ sept5_routes.js
@@ -10,6 +10,33 @@ const server = http.createServer((req, res) => {
         console.log('Funcionando');
         res.end('Funcionando');
     }
+    else if (pathname === '/testDB') {
+        connection.query('SELECT 1', (err, result => {
+
+        }));
+        res.end('Bases de datos probada');
+    }
+    else if (pathname === '/usuarios' && req.method === 'GET') {
+        connection.query('SELECT * FROM usuarios', (err, result) => {
+            if (err) {
+                res.end('Hubo un error em la consulta');
+                return;
+            }
+            result.push({ id: 10 });
+            console.log(result);
+            res.end(result);
+        });
+    }
+    else if (pathname === '/usuario' && req.method === 'POST') {
+        const body = '';
+        req.on('data', chunk => {
+            //body = body + chunk
+            body += chunk;
+        });
+        req.on('end', () => { });
+        
+    }
+
 });
 
 server.listen(3000, () => {
